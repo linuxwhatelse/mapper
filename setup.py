2a0086a22303759f56593cb7dfede083b8d8a6d4
Tadly <jan@tadly.de>
2016-06-07 21:28:51 +0200

    version bump to 1.1.0

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ with open(path.join(here, 'README.md'), encoding='utf-8') as f:
 
 setup(
     name='mapper',
-    version='1.0.0',
+    version='1.1.0',
     description='A simple URL-Scheme resolver',
     long_description=long_description,
     url='https://github.com/linuxwhatelse/mapper',
